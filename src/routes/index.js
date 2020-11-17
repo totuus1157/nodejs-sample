399fc8ec378878e37ab7310331f429acdfe4bfd6
totuus1157 <takahatam@icloud.com>
2020-11-17 17:21:39 +0900

    Tanksテーブルを表示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.Tanks.findAll().then(tanks => {
+    const data = {
+      title: '大洗女子学園 所有戦車リスト',
+      content: tanks
+    }
+  	res.render('index', data);
+  });
 });
 
 module.exports = router;
