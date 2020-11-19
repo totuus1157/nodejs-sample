5847a865cb29a3c3435a437352ccbb395864a6dd
totuus1157 <takahatam@icloud.com>
2020-11-19 10:30:47 +0900

    レコードの新規作成機能を追加

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,4 +13,49 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/add', (req, res,next) => {
+  const data = {
+    title: '新規追加'
+  }
+  res.render('add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  db.sequelize.sync().then(() => db.Tanks.create({
+    name: req.body.name,
+    crew: req.body.crew,
+    length: req.body.length,
+    weight: req.body.weight,
+    speed: req.body.speed,
+    team: req.body.team
+  })).then(tanks => {
+    res.redirect('/');
+  });
+});
+
+router.get('/edit', (req, res, next) => {
+  db.Tanks.findByPk(req.query.id).then(tanks => {
+      const data = {
+        title: '更新',
+        form: tanks
+      }
+    res.render('edit', data);
+  });
+});
+
+router.post('/edit', (req, res, next) => {
+  db.sequelize.sync().then(() => db.Tanks.update({
+    name: req.body.name,
+    crew: req.body.crew,
+    length: req.body.length,
+    weight: req.body.weight,
+    speed: req.body.speed,
+    team: req.body.team
+  },{
+    where: {id:req.body.id}
+  })).then(tanks => {
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
