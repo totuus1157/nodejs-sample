5e3c2820d2be990e552be64cd76690dd339e437d
totuus1157 <takahatam@icloud.com>
2020-11-20 11:03:28 +0900

    バリデーションのエラーメッセージを日本語化した

--- src/models/tanks.js
+++ src/models/tanks.js
@@ -17,45 +17,77 @@ module.exports = (sequelize, DataTypes) => {
     name: {
 	    type: DataTypes.STRING,
 	    validate: {
-	      notEmpty: true
+	      notEmpty: {
+          msg: "車輌名は必ず入力してください。"
+        }
 	    }
   	},
     crew: {
       type: DataTypes.INTEGER,
       validate: {
-        notEmpty: true,
-        isInt: true,
-        min: 1
+        notEmpty: {
+          msg: "搭乗員数は必ず入力してください。"
+        },
+        isInt: {
+          msg: "整数を入力してください。"
+        },
+        min: {
+          args: [1],
+          msg: "１以上の値が必要です。"
+        }
       }
     },
     length: {
       type: DataTypes.FLOAT,
       validate: {
-        notEmpty: true,
-        isFloat: true,
-        min: 0
+        notEmpty: {
+          msg: "全長は必ず入力してください。"
+        },
+        isFloat: {
+          msg: "数値を入力してください。"
+        },
+        min: {
+          args: [0],
+          msg: "０以上の値が必要です。"
+        }
       }
     },
     weight: {
       type: DataTypes.FLOAT,
       validate: {
-        notEmpty: true,
-        isFloat: true,
-        min: 0
+        notEmpty: {
+          msg: "重量は必ず入力してください。"
+        },
+        isFloat: {
+          msg: "数値を入力してください。"
+        },
+        min: {
+          args: [0],
+          msg: "０以上の値が必要です。"
+        }
       }
     },
     speed: {
       type: DataTypes.FLOAT,
       validate: {
-        notEmpty: true,
-        isFloat: true,
-        min: 0
+        notEmpty: {
+          msg: "最高速度は必ず入力してください。"
+        },
+        isFloat: {
+          msg: "数値を入力してください。"
+        },
+        min: {
+          args: [0],
+          msg: "０以上の値が必要です。"
+        }
       }
     },
     team: {
       type: DataTypes.STRING,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: "チーム名は必ず記入してください。"
+        }
       }
     }
   }, {
